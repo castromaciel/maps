6c8862e42be30850aad164c8e9b6df570d6762e2
castromaciel <castromaciel00@gmail.com>
2023-02-05 00:25:42 -0300

    feat: Add fly and activate selected place

--- src/components/SearchResults/SearchResults.tsx
+++ src/components/SearchResults/SearchResults.tsx
@@ -1,9 +1,15 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { PlacesContext } from '../../context'
 import { SearchResultsItem, SkeletonPlaces } from '../index'
 
 const SearchResults = () => {
   const { places, isLoadingPlaces } = useContext(PlacesContext)
+  const [activeId, setActiveId] = useState('')
+
+  const params = {
+    activeId,
+    setActiveId
+  }
 
   if (isLoadingPlaces) return <SkeletonPlaces length={3} />
 
@@ -12,7 +18,7 @@ const SearchResults = () => {
   return (
     <ul className="list-group mt-3">
       {
-        places.map((place) => (<SearchResultsItem key={place.id} place={place} />))
+        places.map((place) => (<SearchResultsItem key={place.id} place={place} {...params} />))
       }
     </ul>
   )
--- src/components/SearchResultsItem/SearchResultsItem.tsx
+++ src/components/SearchResultsItem/SearchResultsItem.tsx
@@ -1,14 +1,46 @@
-import { FC } from 'react'
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+import {
+  Dispatch, FC, SetStateAction, useContext
+} from 'react'
+import { MapContext } from '../../context'
 import { Feature } from '../../interfaces/places'
+import styles from './SearchResultsItem.module.scss'
 
-const SearchResultsItem: FC<{place: Feature}> = ({ place }) => (
-  <li key={place.id} className="list-group-item list-group-item-action">
-    <h6>{place.text}</h6>
-    <p className="text-muted" style={{ fontSize: 12 }}>{place.place_name}</p>
-    <button type="button" className="btn btn-outline-dark">
-      Direcciones
-    </button>
-  </li>
-)
+interface Props {
+  place: Feature
+  activeId: string
+  setActiveId: Dispatch<SetStateAction<string>>
+}
+
+const SearchResultsItem: FC<Props> = ({ place, activeId, setActiveId }) => {
+  const { map } = useContext(MapContext)
+
+  const onPlaceClicked = (selectedPlace: Feature) => {
+    const [lng, lat] = selectedPlace.center
+    map?.flyTo({
+      zoom: 14,
+      center: [lng, lat]
+    })
+    setActiveId(place.id)
+  }
+
+  return (
+    <li
+      key={place.id}
+      className={`${activeId === place.id ? styles.active : ''} list-group-item ${styles.pointer}`}
+      onClick={() => onPlaceClicked(place)}
+    >
+      <h6>{place.text}</h6>
+      <p style={{ fontSize: 12 }}>{place.place_name}</p>
+      <button
+        type="button"
+        className={`${activeId === place.id ? 'btn-outline-light' : 'btn-outline-dark'} btn `}
+      >
+        Direcciones
+      </button>
+    </li>
+  ) 
+}
 
 export default SearchResultsItem
